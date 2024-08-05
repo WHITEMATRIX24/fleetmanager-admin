6d006681ea88fe0c48e72046e1ab16e015c78386
sweetysara <sweetysarasunny@gmail.com>
2024-08-05 19:10:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import TotalVehicle from './components/TotalVehicle';
